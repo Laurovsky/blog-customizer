e01e2eec1f3ed7db997e7925ead58198e8b3b0da
Mark Laurovsky <dead666evil@gmail.com>
2026-07-23 09:04:22 +0300

    переделал стейт на объект

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,20 +2,22 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import fonts from '../../fonts/font.scss';
 import { useState } from 'react';
 import { Select } from 'src/ui/select';
-import { backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions, OptionType } from 'src/constants/articleProps';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [selectedFont, setSelectedFont] = useState(defaultArticleState.fontFamilyOption);
-	const [selectedFontSize, setSelectedFontSize] = useState(defaultArticleState.fontSizeOption);
-	const [selectedFontColor, setSelectedFontColor] = useState(defaultArticleState.fontColor);
-	const [selectedBackgroundColors, setSelectedBackgroundColors] = useState(defaultArticleState.backgroundColor);
-	const [сontentWidth, setСontentWidth] = useState(defaultArticleState.contentWidth);
+	const [formState, setFormState] = useState(defaultArticleState);
 
 	return (
 		<>
@@ -31,43 +33,58 @@ export const ArticleParamsForm = () => {
 				}`}>
 				<form className={styles.form}>
 					<h1 className={styles.formTitle}>ЗАДАЙТЕ ПАРАМЕТРЫ</h1>
-					<Select 
-					selected={selectedFont} 
-					options={fontFamilyOptions}
-					onChange={setSelectedFont}
-					title='шрифт'
-					>
-					</Select>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => {
+							setFormState((prev) => ({
+								...prev,
+								fontFamilyOptions: option,
+							}));
+						}}
+						title='шрифт'></Select>
 					<RadioGroup
-					name=''
-					options={fontSizeOptions}
-					selected={selectedFontSize}
-					onChange={setSelectedFontSize}
-					title='размер шрифта'
-					>
-					</RadioGroup>
-					<Select 
-					selected={selectedFontColor} 
-					options={fontColors}
-					onChange={setSelectedFontColor}
-					title='цвет шрифта'
-					>
-					</Select>
+						name=''
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => {
+							setFormState((prev) => ({
+								...prev,
+								fontSizeOption: option,
+							}));
+						}}
+						title='размер шрифта'></RadioGroup>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => {
+							setFormState((prev) => ({
+								...prev,
+								fontColor: option,
+							}));
+						}}
+						title='цвет шрифта'></Select>
 					<Separator></Separator>
-					<Select 
-					selected={selectedBackgroundColors} 
-					options={backgroundColors}
-					onChange={setSelectedBackgroundColors}
-					title='цвет фона'
-					>
-					</Select>
-					<Select 
-					selected={сontentWidth} 
-					options={contentWidthArr}
-					onChange={setСontentWidth}
-					title='ширина контента'
-					>
-					</Select>
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => {
+							setFormState((prev) => ({
+								...prev,
+								backgroundColor: option,
+							}));
+						}}
+						title='цвет фона'></Select>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => {
+							setFormState((prev) => ({
+								...prev,
+								contentWidth: option,
+							}));
+						}}
+						title='ширина контента'></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
